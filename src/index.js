b3d8d9ff2c2dc22455293b3660c4a0774254ea75
sabazusi <kouhei.live@gmail.com>
2017-01-27 18:58:42 +0900

    Generate image with imagemagick

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hai');
+import im from 'imagemagick';
+
+im.convert(['-size', '128x128', 'xc:#ff0000', 'test.jpg'], (err, stdout) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(stdout);
+  }
+});
