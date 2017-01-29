cb53ff944999b5891cc5d1c594ba2f047547f4cc
sabazusi <kouhei.live@gmail.com>
2017-01-29 23:57:28 +0900

    Add image creation question

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import im from 'imagemagick';
+import inquirer from 'inquirer';
 
 im.convert(['-size', '128x128', 'xc:#ff0000', 'test.jpg'], (err, stdout) => {
   if (err) {
@@ -7,3 +8,31 @@ im.convert(['-size', '128x128', 'xc:#ff0000', 'test.jpg'], (err, stdout) => {
     console.log(stdout);
   }
 });
+
+
+const emptyValidator = (name) => {
+  if (!name) return 'Empty input!';
+  return true;
+};
+
+/**
+ * type: create image with strings
+ *
+ */
+const createImageQuestions = [
+  {
+    type: 'input',
+    name: 'outputFileName',
+    message: 'Input new image file name',
+    validate: emptyValidator
+  },
+  {
+    type: 'list',
+    name: 'outputFileType',
+    message: 'Select image file type',
+    choices: ['jpg', 'png']
+  }
+];
+
+inquirer.prompt(createImageQuestions)
+  .then((answer) => console.log(answer));
