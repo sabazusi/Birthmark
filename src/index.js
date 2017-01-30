f27b3f9564acf4ca775489ad38bd7c6610fac146
sabazusi <kouhei.live@gmail.com>
2017-01-30 22:53:52 +0900

    Add questions for embed text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import im from 'imagemagick';
 import inquirer from 'inquirer';
 
+/**
 im.convert(['-size', '128x128', 'xc:#ff0000', 'test.jpg'], (err, stdout) => {
   if (err) {
     console.log(err);
@@ -8,6 +9,7 @@ im.convert(['-size', '128x128', 'xc:#ff0000', 'test.jpg'], (err, stdout) => {
     console.log(stdout);
   }
 });
+*/
 
 
 const emptyValidator = (name) => {
@@ -31,6 +33,24 @@ const createImageQuestions = [
     name: 'outputFileType',
     message: 'Select image file type',
     choices: ['jpg', 'png']
+  },
+  {
+    type: 'input',
+    name: 'embedText',
+    message: 'Input string that embed in image',
+    validate: emptyValidator
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Input text color code for image',
+    "default": '#000000'
+  },
+  {
+    type: 'input',
+    name: 'backgroundColor',
+    message: 'Input background color code for image',
+    "default": '#ffffff'
   }
 ];
 
