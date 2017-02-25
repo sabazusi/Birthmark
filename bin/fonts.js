4db345e7563673b5a4b98ecaeb5f1b3c780559e0
sabazusi <kouhei.live@gmail.com>
2017-02-25 11:48:11 +0900

    Allocate fonts by initial character

--- bin/fonts.js
+++ bin/fonts.js
@@ -17,8 +17,51 @@ var availableFonts = _fontManager2.default.getAvailableFontsSync().map(function 
   };
 });
 
+var rangedStartsWith = function rangedStartsWith(target, range) {
+  return range.some(function (p) {
+    return target.name.toUpperCase().startsWith(p.toUpperCase());
+  });
+};
+
+var allocate = function allocate(fonts) {
+  var allocated = {
+    'a-c': [],
+    'd-f': [],
+    'g-i': [],
+    'j-l': [],
+    'm-o': [],
+    'p-r': [],
+    's-u': [],
+    'v-z': [],
+    'others': []
+  };
+  fonts.forEach(function (f) {
+    if (rangedStartsWith(f, ['a', 'b', 'c'])) {
+      allocated['a-c'].push(f);
+    } else if (rangedStartsWith(f, ['d', 'e', 'f'])) {
+      allocated['d-f'].push(f);
+    } else if (rangedStartsWith(f, ['g', 'h', 'i'])) {
+      allocated['g-i'].push(f);
+    } else if (rangedStartsWith(f, ['j', 'k', 'l'])) {
+      allocated['j-l'].push(f);
+    } else if (rangedStartsWith(f, ['m', 'n', 'o'])) {
+      allocated['m-o'].push(f);
+    } else if (rangedStartsWith(f, ['p', 'q', 'r'])) {
+      allocated['p-r'].push(f);
+    } else if (rangedStartsWith(f, ['s', 't', 'u'])) {
+      allocated['s-u'].push(f);
+    } else if (rangedStartsWith(f, ['v', 'w', 'x', 'y', 'z'])) {
+      allocated['v-z'].push(f);
+    } else {
+      allocated['others'].push(f);
+    }
+  });
+  return allocated;
+};
+
 var font = {
-  availables: availableFonts
+  availables: availableFonts,
+  allocated: allocate(availableFonts)
 };
 
 exports.default = font;
--- src/fonts.js
+++ src/fonts.js
@@ -8,8 +8,49 @@ const availableFonts = fontManager.getAvailableFontsSync()
     }
   });
 
+const rangedStartsWith = (target, range) => {
+  return range.some(p => target.name.toUpperCase().startsWith(p.toUpperCase()));
+};
+
+const allocate = (fonts) => {
+  const allocated = {
+    'a-c': [],
+    'd-f': [],
+    'g-i': [],
+    'j-l': [],
+    'm-o': [],
+    'p-r': [],
+    's-u': [],
+    'v-z': [],
+    'others': []
+  };
+  fonts.forEach(f => {
+    if (rangedStartsWith(f, ['a', 'b', 'c'])) {
+      allocated['a-c'].push(f);
+    } else if (rangedStartsWith(f, ['d', 'e', 'f'])) {
+      allocated['d-f'].push(f);
+    } else if (rangedStartsWith(f, ['g', 'h', 'i'])) {
+      allocated['g-i'].push(f);
+    } else if (rangedStartsWith(f, ['j', 'k', 'l'])) {
+      allocated['j-l'].push(f);
+    } else if (rangedStartsWith(f, ['m', 'n', 'o'])) {
+      allocated['m-o'].push(f);
+    } else if (rangedStartsWith(f, ['p', 'q', 'r'])) {
+      allocated['p-r'].push(f);
+    } else if (rangedStartsWith(f, ['s', 't', 'u'])) {
+      allocated['s-u'].push(f);
+    } else if (rangedStartsWith(f, ['v', 'w', 'x', 'y', 'z'])) {
+      allocated['v-z'].push(f);
+    } else {
+      allocated['others'].push(f);
+    }
+  });
+  return allocated;
+};
+
 const font = {
-  availables: availableFonts
+  availables: availableFonts,
+  allocated: allocate(availableFonts)
 };
 
 export default font;
